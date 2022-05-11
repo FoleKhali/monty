dc5ff3e3a0c0848e982b43070b218bc75ed8798d
FoleKhali <platanobrgas@gmail.com>
2022-05-11 13:08:16 -0500

    try betty

--- get_func.c
+++ get_func.c
@@ -2,29 +2,17 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * @f: ...
  */
 
-void (*f)(stack_t **stack, unsigned int line_number);
-	instruction_T [] {
-	
+void (*f)(stack_t **stack, unsigned int line_number)
+{
+	instruction_T [] {	
 		{"push", push_handler},
 		{"pall", pall_handler},
 		{"pint", pint_handler},
 		{"pop", pop_handler},
 		{"nop", nop_handler},
 		{"sub", sub_handler},
-	}	
-
-/**
- *
- *
- *
- */
-
-void push_handler(stack_t **stack, unsigned int line_number)
-{
-	(void)line_number;
+	}
 }
