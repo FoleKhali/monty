a686aa8392c9c2d4a4ee55e856064165333e50ac
FoleKhali <platanobrgas@gmail.com>
2022-05-12 13:31:25 -0500

    final result of the opcode.c

--- opcode.c
+++ opcode.c
@@ -2,28 +2,44 @@
 #include "monty.h"
 
 /**
- * @f: ...
+ * get_func - selects the right function
+ * @parsed: line from the bytecode file passed to main
+ *
+ * Return: pointer to the selected function, or NULL on failure
  */
-
-void the_opcode(stack_t **stack, char *argu)
+void (*get_func(char **parsed))(stack_t **, unsigned int)
 { 
-	instruction_t code[] = {
+	instruction_t fun_arr[] = {
 		{"push", push_handler},
 		{"pall", pall_handler},
 		{"pint", pint_handler},
 		{"pop", pop_handler},
+		{"swap", swap_handler},
+		{"add", add_handler},
 		{"nop", nop_handler},
 		{"sub", sub_handler},
+		{"div", div_handler},
+		{"mul", mul_handler},
+		{"mod", mod_handler},
+		{"pchar", pchar_handler},
+		{"pstr", pstr_handler},
+		{"rotl", rotl_handler},
+		{"rotr", rotr_handler},
+		{"stack", stack_handler},
+		{"queue", queue_handler},
 		{NULL, NULL}
 	};
 
-	int x = 0;
+	int codes = 17, i;
 
-	while (code[x].opcode)
+	for (i = 0; i < codes; i++)
 	{
-		if (strcmp())
+		if (strcmp(func_arr[i].opcode, parsed[0]) == 0)
+		{
+			return (func_arr[i].f);
+		}
 	}
-
+	return (NULL);
 }
 
 /**
@@ -48,38 +64,53 @@ stack_t *n_node(int n)
 }
 
 /**
- * @f: ...
+ * push_handler - handles the push instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
  */
-
 void push_handler(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new = NULL;
-	(void)line_number;
+	stack_t *new;
+	int num = 0, i;
 
-	new = n_node(result);
-
-	new->next = *stack;
-	if (*stack != NULL)
+	if (data.words[1] == NULL)
 	{
-		(*stack)->prev = new;
+		dprintf(STDERR_FILENO, PUSH_FAIL, line_number);
+		free_all(1);
+		exit(EXIT_FAILURE);
 	}
-	*stack = new;
+
+	for (i = 0; data.words[1][i]; i++)
+	{
+		if (isalpha(data.words[1][i]) != 0)
+		{
+			dprintf(STDERR_FILENO, PUSH_FAIL, line_number);
+			free_all(1);
+			exit(EXIT_FAILURE);
+		}
+	}
+	num = atoi(data.words[1]);
+
+	if (data.qflag == 0)
+		new = add_dnodeint(stack, num);
+	else if (data.qflag == 1)
+		new = add_dnodeint_end(stack, num);
+	if (!new)
+	{
+		dprintf(STDERR_FILENO, MALLOC_FAIL);
+		free_all(1);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
- * @f: ...
+ * pall_handler - handles the pall instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
  */
-
 void pall_handler(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 	if (*stack)
 		print_dlistint(*stack);
 }
-
-void nop_handler(stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
-}
-
